ff32c09da720a32f9f5023a9ab7bc52d38b8a07a
agwanyanjaba <bonkewycliffe@yhoo.com>
2020-07-12 17:49:23 +0300

    added msg api

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return "Wycliffe Hello World!"
+
+@app.route('/msg')
+def hello():
+    return "Wycliffe Hello World!"
